e9e36a42c4d8c7a44fa0f92f75a9108381139c0e
GrandMasterNerd <165722873+GrandMasterNerd@users.noreply.github.com>
2025-01-25 15:11:35 +0000

    Added new title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+#st.title("🎈 My new app")
+st.title("Compass Chronicles: Kingston")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.write("Hello world")
+
