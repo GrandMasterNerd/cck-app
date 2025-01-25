25c59af884d7931ff3373b86d0a14636de456e17
GrandMasterNerd <165722873+GrandMasterNerd@users.noreply.github.com>
2025-01-25 15:45:55 +0000

    Test run

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,5 @@ import streamlit as st
 
 #st.title("🎈 My new app")
 st.title("Compass Chronicles: Kingston")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.write("Hi Nolan")
+st.write("Hello world")
 st.image("res/b-BeamishMunro.jpg")
