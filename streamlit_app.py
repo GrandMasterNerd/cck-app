e9851e48611c652709331490b8218b4fa9e7ec6e
GrandMasterNerd <165722873+GrandMasterNerd@users.noreply.github.com>
2025-01-25 15:25:44 +0000

    Added message

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.title("Compass Chronicles: Kingston")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-
+st.write("Hi Nolan")
