814a7e0bf351c4c49633118e757547a423006b25
GrandMasterNerd <165722873+GrandMasterNerd@users.noreply.github.com>
2025-01-25 16:57:30 +0000

    Removed changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,35 +25,6 @@ st.markdown(
     """, unsafe_allow_html=True
 )
 
-# Add custom CSS to change background color and hide header/footer elements
-st.markdown(
-    """
-    <style>
-    body, .block-container {
-        background-color: #FF10F0 !important;  # Neon Pink color code
-        margin: 0 !important;  # Remove default margin
-        padding: 0 !important;  # Remove default padding
-    }
-
-    /* Hide the header */
-    .css-1d391kg {  
-        display: none !important;  
-    }
-
-    /* Hide the footer */
-    .css-1k8p3ln { 
-        display: none !important;
-    }
-
-    /* Hide the GitHub button at the bottom */
-    .css-1s7veaj {  
-        display: none !important;
-    }
-
-    </style>
-    """, unsafe_allow_html=True
-)
-
 # st.title("🎈 My new app")
 st.title("Compass Chronicles: Kingston")
 st.write("Welcome to Compass Chronicles: Kingston")
