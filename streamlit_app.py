89c07e028b00c74ceee0ed8870d43e7e11ffcba1
GrandMasterNerd <165722873+GrandMasterNerd@users.noreply.github.com>
2025-01-25 16:47:41 +0000

    Changed background color

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,29 @@
+#
+# Streamlight App
+# Authors: Nolan Verboomen, Adam Likogiannis, Eiqan Ahsan 
+#          and Aidan Murray
+# Purpose: App connects to "beacons" placed at iconic landmarks
+#          around the city. Using HTTPS messages, sends data
+#          to the app which lets the app know which
+#          landmark the user is at. App shows the user 
+#          the history of the landmark, including 
+#          pictures, videos, audio, etc.
+#
+
 import streamlit as st
 
-#st.title("🎈 My new app")
+# Add custom CSS to change the background color to Prussian Blue
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #003153;  # Prussian Blue color code
+    }
+    </style>
+    """, unsafe_allow_html=True
+)
+
+# st.title("🎈 My new app")
 st.title("Compass Chronicles: Kingston")
 st.write("Welcome to Compass Chronicles: Kingston")
 st.image("res/Kingston-Overview.JPG")
